a97c68a38ddac62e6867fa5c131c700fd0db6143
Raúl González <rafex@rafex.dev>
2023-03-18 15:58:52 -0600

    pruebas de compilación

--- dockers/graalvm/buid.sh
+++ dockers/graalvm/buid.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DOCKERFILE=Dockerfile_graalvm
-SRC_PATH=/home/rafex/github/gist/dockers/graalvm/src
-PATH_IN_DOCKER=/usr/src/graalvmExample
-NAME_VOLUME=graalvm-example
+SRC_PATH=/src
+PATH_IN_DOCKER=/src2
+NAME_BUILD=graalvm_example:v01
 
-docker build . -t graalvm_example:v01 -f $DOCKERFILE
+docker build . -t $NAME_BUILD -f $DOCKERFILE
 
-docker run -it --rm --mount type=bind,source=$SRC_PATH,target=$PATH_IN_DOCKER $NAME_VOLUME
+docker run -it --rm --mount type=bind,source="$PWD"$SRC_PATH,target=$PATH_IN_DOCKER $NAME_BUILD bash
+# docker run -it --rm $NAME_BUILD bash
