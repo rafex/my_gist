73a812e8586e7cc4abd9c4b43ad52a95805886eb
Raúl González <rafex@rafex.dev>
2023-06-28 09:52:10 -0600

    creando instalador de maven

---
+++ install_maven_debian/install_maven_debian.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+# @Author Raúl González
+# Twitter @rafex
+
+CONFIGURATION_JSON="install_maven_debian.json"
+TMP_PATH="/tmp/maven"
+INSTALLATION_PATH=$(jq '.installationPath' $CONFIGURATION_JSON)
+INSTALLATION_PATH=$(echo $INSTALLATION_PATH | tr -d '\"')
+SUDO=$(jq '.sudo' $CONFIGURATION_JSON)
+
+
+if [ $SUDO -eq 1 ]
+  then
+    echo "Sudo activate"
+    SUDO="sudo"
+  else
+    echo "Sudo desactivate"
+    SUDO=""
+fi
+
+# Example URL
+# https://github.com/ibmruntimes/semeru17-binaries/releases/download/jdk-17.0.6%2B10_openj9-0.36.0/ibm-semeru-open-jdk_x64_linux_17.0.6_10_openj9-0.36.0.tar.gz
+
+
+download() {
+  resources=$(echo $1 | tr -d '\"')
+  if [ ! -d "$TMP_PATH" ]; then
+    echo "$TMP_PATH does not exist."
+    mkdir -p $TMP_PATH
+  fi
+  wget -P $TMP_PATH -c $resources
+}
+
+unpackage() {
+  folder="$INSTALLATION_PATH/$1"
+  folder=$(echo $folder | tr -d '\"')
+  if [ ! -d "$folder" ]; then
+    echo "$folder does not exist."
+    $SUDO mkdir -p $folder
+  fi
+  binary=$2
+  binary=$(echo $binary | tr -d '\"')
+  $SUDO tar -xf "$TMP_PATH/$binary" -C $folder --strip-components=1
+}
+
+install_update_alternatives() {
+  name=$(echo $1 | tr -d '\"')
+  path=$(echo $2 | tr -d '\"')
+  priority=$(echo $3 | tr -d '\"')
+  #echo $path
+  $SUDO update-alternatives --install /usr/bin/$name $name $INSTALLATION_PATH/$path/bin/$name $
+  update-alternatives --list $name
+}
+
+count=0 
+while [ $count -lt 100 ]; do
+  version=$(jq .versions[$count].version $CONFIGURATION_JSON)
+  if [ $version == "null" ]
+    then
+      echo "No more versions"
+      
+      exit 0
+  fi
+
+  ignore=$(jq .versions[$count].ignore $CONFIGURATION_JSON)
+  install=$(jq .install $CONFIGURATION_JSON)
+
+  if [ $ignore -eq 0 ]; then
+    if [ $install == $version ] || [ $install -eq "all" ]; then
+      echo "Version of count is: $count - [$version] - version [$(jq .versions[$count].version $CONFIGURATION_JSON)] - arch [$(jq .versions[$count].arch $CONFIGURATION_JSON)]"
+      url=$(jq .urlBin $CONFIGURATION_JSON)
+      url=$(echo $url | sed 's/""//')
+      priority=$(jq .versions[$count].priority $CONFIGURATION_JSON)
+      name=$(jq .versions[$count].package $CONFIGURATION_JSON)
+      name=$(echo $LINE | sed -e "s/VERSION/${version}/g")
+      name=$(echo "${name%.*.*}")
+      path="$(jq .versions[$count].version $CONFIGURATION_JSON)/$(jq .versions[$count].arch $CONFIGURATION_JSON)/$name"
+      package=$(jq .versions[$count].package $CONFIGURATION_JSON)
+      package=$(echo $LINE | sed -e "s/VERSION/${version}/g")
+      download "$url/$package"
+      unpackage $path $package      
+
+      count_bin=0
+      while [ $count_bin -lt 10 ]; do
+        bin=$(jq .versions[$count].binarys[$count_bin] $CONFIGURATION_JSON)
+        if [ $bin == "null" ]
+          then
+            echo ""
+          else 
+            install_update_alternatives $bin $path $priority            
+        fi
+        count_bin=$(($count_bin + 1))
+      done  
+    fi
+        
+  fi
+  
+  count=$(($count + 1))
+done
+
+
